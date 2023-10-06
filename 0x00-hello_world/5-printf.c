bcad0b1161f480042507eb707ad37adf8d4979ec
b0b0blarrrrr <babalolaoluwatobi6@gmail.com>
2023-10-06 16:26:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,9 @@
  * main - This is a C program that uses the printf function to print exactly
  * Return: 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
+}
