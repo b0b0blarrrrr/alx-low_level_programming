d1757952848442ca2c51efe058e6e70960deee61
b0b0blarrrrr <babalolaoluwatobi6@gmail.com>
2023-10-06 16:01:57 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a C program that uses the function puts to print exactly
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
