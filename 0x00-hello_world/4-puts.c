e6c9330b8e5d4151a041eed5fce35a947220218d
b0b0blarrrrr <babalolaoluwatobi6@gmail.com>
2024-06-20 16:11:13 -0700

    2-assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *  main - Entry point*
- *
+ *  main - A program that prints a line with the puts function
  *  Return: Always 0 (Success)
  *  /
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
